e4cce262fcceacb2c303e4eb2be1c6c98e1f3bde
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-05 19:28:17 +0700

    Add files via upload

---
+++ Program mengubah bilangan pecahan menjadi bilangan bulat.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+int p1,b1,b2;
+
+cout<<"PROGRAM MERUBAH BILANGAN PECAHAN KE BILANGAN BULAT"<<endl;
+cout<<"Masukan Pembilang = ";
+cin>>b1;
+cout<<"Masukan Penyebut  = ";
+cin>>b2;
+cout<<"\                 "<<b1<<endl;
+cout<<"\           p1=   -"<<endl;
+cout<<"\                 "<<b2<<endl;
+p1=b1/b2;
+if("/")
+{
+    cout<<"Hasilnya Adalah = "<<p1<<endl;
+}
+return 0;
+
+
+}
